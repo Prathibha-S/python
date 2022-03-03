94d3fbbd0b6acfa11d6013b609fafa4ac2d5bdf8
Prathibha <95700454+Prathibha-S@users.noreply.github.com>
2022-03-03 22:53:48 +0530

    Add files via upload

---
+++ class3.py
@@ -0,0 +1,22 @@
+class Rectangle:
+	def __init__(self, a, b):
+		self.length = a
+		self.breadth = b
+	def area(self):
+		return self.length * self.breadth
+	def __lt__(self, o):
+    		if self.area() < o.area():
+        		return True
+    		else:
+        		return False
+
+h1 = int(input("Enter length of first rectangle : "))
+b1 = int(input("Enter breadth of first rectangle : "))
+h2 = int(input("Enter length of second rectangle : "))
+b2 = int(input("Enter breadth of second rectangle : "))
+rect1 = Rectangle(h1, b1)
+rect2 = Rectangle(h2, b2)
+if rect1 < rect2:
+	print("Rectangle 2 is greater than Rectangle 1")
+else:
+	print("Rectangle 1 is greater than Rectangle 2")
