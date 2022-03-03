70c61e834fbbd120d55e134a5744891c7c987695
Prathibha <95700454+Prathibha-S@users.noreply.github.com>
2022-03-03 22:58:39 +0530

    Add files via upload

--- file2.py
+++ file2.py
@@ -1,5 +1,8 @@
-f=[]
-file=input("Enter the file name:")
-f=file.split(".")
-print(f)
-print("extension of the file is:"+f[-1])
+import csv
+with open('data.csv','r') as csvfile:
+ data = csv.DictReader(csvfile)
+ print("Appeared  Designed By")
+ print("---------------------------------")
+ for row in data:
+   print(row['Appeared'] ,row['Designed by'])
+
