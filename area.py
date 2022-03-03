3cd51c9f9150ae3f18c86d8e22aca723b0315419
Prathibha <95700454+Prathibha-S@users.noreply.github.com>
2022-03-03 22:59:57 +0530

    Add files via upload

--- area.py
+++ area.py
@@ -1,6 +1,22 @@
-a=int(input("Enter the first number: "))
-b=int(input("Enter the second number: "))
-c=int(input("Enter the third number: "))
-s=(a+b+c)/2
-area=(s*(s-a)+(s-b)+(s-c))**0.5
-print("The area is:%0.2f"%area)
+from Graphics.RectFunctions import*
+from Graphics.CirFunctions import *
+from Graphics.DGraphics.SphereFunctions import *
+from Graphics.DGraphics.CuboidFunctions import *
+l=int(input("Enter the length of Rectangle:"))
+b=int(input("Enter the breadth of Rectangle:"))
+print("area = ",RectArea(l,b))
+print("Perimeter =", RectPerimeter(l,b))
+
+r=int(input("Enter the radius of a circle:"))
+print("Circle area = ",CirArea(r))
+print("Circle Perimeter =", CirPerimeter(r))
+
+r=int(input("Enter the radius of Sphere:"))
+print("Sphere area = ",SpArea(r))
+print("Sphere Perimeter =", SpPerimeter(r))
+
+l=int(input("Enter the  length of Cuboid:"))
+b=int(input("Enter the breadth of Cuboid:"))
+h=int(input("Enter the height of Cuboid:"))
+print("Cuboid area = ",CuboidArea(l,b,h))
+print("Cuboid Perimeter =",CuboidPerimeter(l,b,h))
